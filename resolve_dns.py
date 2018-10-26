6987914776dc078fdab440e61072ebb10ff41c52
Henry Birge-Lee <henrybirgelee@gmail.com>
2018-10-26 11:32:33 -0700

    Added read certificate history.

--- resolve_dns.py
+++ resolve_dns.py
@@ -1,4 +1,32 @@
 # Resolve certificates for cert database.
 import routing_aware_dns_resolver as rad
+import MySQLdb as db
+import time
+
+import os,sys
+
+# Code to use external read certificate history.
+#import imspect
+#currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+#parentdir = os.path.dirname(currentdir)
+#bgpfpstudy = parentdir + "/BGP-age-false-positive-study"
+#sys.path.insert(0,bgpfpstudy) 
+#from read_certificate_history import getNextCertificate
+
+from read_certificate_history_unordered import getNextCertificate
+
+
 
 print(rad.lookupA("example.com"))
+
+
+scriptPath = os.path.dirname(os.path.realpath(__file__))
+
+
+lastCertificateIndexProcessedFileLocation = scriptPath + "/last-certificate-index-processed.var"
+
+lastCertificateIndexProcessedFile = open(lastCertificateIndexProcessedFileLocation, 'wb')
+
+while True:
+  certificate = getNextCertificate(lastCertificateIndexProcessedFile)
+  print(certificate["commonName"])
