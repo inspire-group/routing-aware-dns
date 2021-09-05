93068b466f700619379037cc1a603cee825c2ce1
Grace Cimaszewski <gc444@cornell.edu>
2021-09-04 23:25:03 -0400

    Separated full lookup and summary logs; added compression.

--- log_processor.py
+++ log_processor.py
@@ -17,14 +17,16 @@ from threading import Lock
 # sample 1 in 1000 certificates
 # single thread with 10 second timeout per domain
 TODAY = date.today().strftime("%Y%m%d")
-BUCKET_NAME = "letsencryptdata"
-LOG_FILE = f'issuance.log.den-{TODAY}.gz'
+DATA_BUCKET_NAME = "letsencryptdata"
+RES_BUCKET_NAME = "letsencryptdnsresults"
+LOG_FILE = "retry-issuance.log.den-20210507.gz" # f'issuance.log.den-{TODAY}.gz'
 LOCAL_LOG_FILE = "local.log.test.gz"
 CRED_PROFILE = "dns_res"
 RES_FILE = f"lookup-results-{TODAY}.txt"
+FULL_LKUP_FILE = f"lookups-archive-{TODAY}.gz"
 LOGGER_FILE = f"log-{TODAY}.log"
 JSON_TAG = "JSON="
-MAX_COUNT = 1000
+MAX_COUNT = 2000
 NUM_REPEAT_LKUPS = 3
 
 logging.basicConfig(filename=LOGGER_FILE, level=logging.DEBUG)
@@ -47,18 +49,18 @@ def add_cert_to_dict(cert_id, pid):
 
 
 def read_log_from_bucket(session, log_file):
-    logging.info(f'Reading log from bucket {BUCKET_NAME}')
+    logging.info(f'Reading log from bucket {DATA_BUCKET_NAME}')
 
     start = time.time()
     s3 = session.resource('s3')
-    bucket = s3.Bucket(BUCKET_NAME)
+    bucket = s3.Bucket(DATA_BUCKET_NAME)
 
     try:
         bucket.download_file(log_file, LOCAL_LOG_FILE)
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == '404':
-            # think of some notification mechanism to put here
-            logging.error(f'The object {log_file} does not exist in bucket {BUCKET_NAME}')
+            # TODO: implement some notification mechanism to put here
+            logging.error(f'The object {log_file} does not exist in bucket {DATA_BUCKET_NAME}')
         else:
             logging.error(f'Download of {log_file} failed')
             raise e
@@ -72,7 +74,9 @@ def process_file(name):
     pass
 
 
-def parse():
+'''Returns a dictionary (cert ID: URLs) of max size cert_count.
+'''
+def parse(cert_count):
 
     cert_url_dict = {}
 
@@ -80,7 +84,7 @@ def parse():
     with gzip.open(LOCAL_LOG_FILE) as f:
         # linecount = sum(1 for line in f)
         for line in f:
-            if len(cert_url_dict) >= MAX_COUNT:
+            if len(cert_url_dict) >= cert_count:
                 break
             l = line.decode()
             cert_log = json.loads(l[l.index(JSON_TAG)+len(JSON_TAG):])
@@ -94,10 +98,13 @@ def parse():
     return cert_url_dict
 
 
+# extract dictionary values from perform full lookup for name
+# two separate log files: one for statistics, one for full lookups
+# add the backup resolver results as well
 def resolve_dns(cert_url_dict):
 
     start1 = time.time()
-    with open(RES_FILE, "a") as f:
+    with open(RES_FILE, "a") as f, gzip.open(FULL_LKUP_FILE, "wb") as f2:
 
         succ_timer = 0.
         failed_timer = 0.
@@ -129,11 +136,22 @@ def resolve_dns(cert_url_dict):
                         url_lkups.append(str(e))
                         # lookups.append(str(e))
                 lookups.append(url_lkups)
-
-            lookup_json = {urls[i]: lookups[i] for i in range(len(urls))}
-            res_json['Authorization_lookups'] = lookup_json
-            results.append(res_json)
+            lookup_json = {}
+            full_lkups = {}
+            for i in range(len(urls)):
+                url_lkup = lookups[i] # contains N lookups for the given URL (checking for path dependence)
+                ipv4_lkup = [_.pop("lookup_ipv4") if isinstance(_, dict) else _ for _ in url_lkup]
+                ipv6_lkup = [_.pop("lookup_ipv6") if isinstance(_, dict) else _ for _ in url_lkup]
+                lookup_json[urls[i]] = url_lkups
+                full_lkups[urls[i]] = (ipv4_lkup, ipv6_lkup)
+            # lookup_json = {urls[i]: lookups[i] for i in range(len(urls))}
+            res_json['lookups'] = lookup_json
+            # results.append(res_json)
+            res_json2 = {}
+            res_json2['ID'] = id_
+            res_json2['lookups'] = full_lkups
             f.write(f'{res_json}\n')
+            f2.write(f'{res_json2}\n'.encode())
     lookup_timer = succ_timer + failed_timer    
     logging.info(f'Total time: {lookup_timer:.4f} sec. performing lookups for {len(cert_url_dict)} certs.')
     logging.info(f'Total of {succ_lookups} successful ({succ_timer:.3f} sec.) and {failed_lookups} ({failed_timer:.3f} sec.) failed lookups.')
@@ -163,13 +181,15 @@ def process_daily_log(args):
     logging.info(f'Performing lookups for log file {log_file}')
     session = boto3.Session(profile_name=CRED_PROFILE)
     read_log_from_bucket(session, log_file)
-    certs = parse()
+    certs = parse(MAX_COUNT)
     lookup_res = resolve_dns(certs)
     logging.info('Successfuly performed DNS lookups')
     return 0
 
 
 def worker(in_q, out_q):
+    # in_q: contains certificates that need to be looked up
+    # out_q: contains lookup results
     name = os.getpid()
     while True:
         item_cert = in_q.get()
@@ -179,7 +199,7 @@ def worker(in_q, out_q):
         lookup = resolve_dns(item_cert)
 
 
-def listener():
+def listener(write_q):
     # listens for lookup messages on the queue and writes to file
     global counter
     pass
