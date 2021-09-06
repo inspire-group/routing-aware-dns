4fe536d59053bc92e9938f00f02a6fdf7fe70317
Grace Cimaszewski <gc444@cornell.edu>
2021-09-06 15:56:24 -0400

    Added multithreaded lookups and logging, copy to archive bucket.

--- log_processor.py
+++ log_processor.py
@@ -10,9 +10,8 @@ from datetime import date
 import time
 import sys
 import logging
-import multiprocessing
+import multiprocessing as mp
 import os
-from threading import Lock
 
 # sample 1 in 1000 certificates
 # single thread with 10 second timeout per domain
@@ -26,7 +25,7 @@ RES_FILE = f"lookup-results-{TODAY}.txt"
 FULL_LKUP_FILE = f"lookups-archive-{TODAY}.gz"
 LOGGER_FILE = f"log-{TODAY}.log"
 JSON_TAG = "JSON="
-MAX_COUNT = 2000
+MAX_COUNT = 10
 NUM_REPEAT_LKUPS = 3
 
 logging.basicConfig(filename=LOGGER_FILE, level=logging.DEBUG)
@@ -35,21 +34,10 @@ logging.getLogger('botocore').setLevel(logging.CRITICAL)
 logging.getLogger('s3transfer').setLevel(logging.CRITICAL)
 logging.getLogger('urllib3').setLevel(logging.CRITICAL)
 
-counter = multiprocessing.Value('i', 0)
-looked_up_cert_ids = {}
-cert_lock = Lock()
-
-
-def add_cert_to_dict(cert_id, pid):
-    cert_lock.acquire()
-    try:
-        looked_up_cert_ids[cert_id] = pid
-    finally:
-        cert_lock.release()
-
 
 def read_log_from_bucket(session, log_file):
     logging.info(f'Reading log from bucket {DATA_BUCKET_NAME}')
+    print(f'Reading log from bucket {DATA_BUCKET_NAME}')
 
     start = time.time()
     s3 = session.resource('s3')
@@ -66,108 +54,56 @@ def read_log_from_bucket(session, log_file):
             raise e
 
     end = time.time()
-    logging.info(f'Took {(end - start):.4f} sec. to download log from S3')
+    logging.info(f'Downloaded log from S3 in {(end - start):.4f} seconds.')
 
 
-def process_file(name):
-    
-    pass
+def write_logs_to_bucket(session):
+
+    start = time.time()
+    print(f'writing logs to bucket')
+    s3 = session.resource('s3')
+    result_bucket = s3.Bucket(RES_BUCKET_NAME)
+    region = session.region_name
+    key_prfx = TODAY + "/" + region + "/"
+
+    result_bucket.upload_file(RES_FILE, key_prfx + "lookups_summary.txt")
+    result_bucket.upload_file(FULL_LKUP_FILE, key_prfx + "full_lookups_archive.gz")
+
+    end = time.time()
+    logging.info(f'Copied lookup result files to S3 bucket {RES_BUCKET_NAME} in {(end - start):.4f} seconds.')
+
+    return 0
 
 
 '''Returns a dictionary (cert ID: URLs) of max size cert_count.
 '''
-def parse(cert_count):
+def parse(manager, cert_count):
 
-    cert_url_dict = {}
-
+    out_q = manager.Queue()
+    certs_seen = set()
     start = time.time()
     with gzip.open(LOCAL_LOG_FILE) as f:
         # linecount = sum(1 for line in f)
         for line in f:
-            if len(cert_url_dict) >= cert_count:
+            if len(certs_seen) >= cert_count:
                 break
             l = line.decode()
             cert_log = json.loads(l[l.index(JSON_TAG)+len(JSON_TAG):])
             urls = list(cert_log['Authorizations'].keys())
             id_code = cert_log['ID']
-            if id_code not in cert_url_dict: # many duplicate lines in logs
-                cert_url_dict[id_code] = urls
+            if id_code not in certs_seen: # many duplicate lines in logs
+                certs_seen.add(id_code)
+                out_q.put((id_code, urls))
 
     stop = time.time()
-    logging.info(f'Took {(stop - start):.4f} sec. to read log file ({len(cert_url_dict)} unique entries parsed for resolving).')
-    return cert_url_dict
-
-
-# extract dictionary values from perform full lookup for name
-# two separate log files: one for statistics, one for full lookups
-# add the backup resolver results as well
-def resolve_dns(cert_url_dict):
-
-    start1 = time.time()
-    with open(RES_FILE, "a") as f, gzip.open(FULL_LKUP_FILE, "wb") as f2:
-
-        succ_timer = 0.
-        failed_timer = 0.
-        logging.debug('Starting lookups!')
-        results = []
-        succ_lookups = 0
-        failed_lookups = 0
-        for id_, urls in cert_url_dict.items():
-            start = time.time()
-            res_json = {}
-            res_json['ID'] = id_
-            lookups = []
-            for url in urls:
-                start = time.time()
-                url_lkups = []
-                for i in range(NUM_REPEAT_LKUPS):
-                    try:
-                        lookup = dns_resolver.perform_full_name_lookup(url)
-                        end = time.time()
-                        succ_timer += (end - start)
-                        succ_lookups += 1
-                        url_lkups.append(lookup)
-                        # lookups.append(lookup)
-                    except Exception as e:
-                        end = time.time()
-                        failed_timer += (end - start)
-                        failed_lookups += 1
-                        logging.debug(f'Failed to resolve domain {url} for ID {id_}: {str(e)}')
-                        url_lkups.append(str(e))
-                        # lookups.append(str(e))
-                lookups.append(url_lkups)
-            lookup_json = {}
-            full_lkups = {}
-            for i in range(len(urls)):
-                url_lkup = lookups[i] # contains N lookups for the given URL (checking for path dependence)
-                ipv4_lkup = [_.pop("lookup_ipv4") if isinstance(_, dict) else _ for _ in url_lkup]
-                ipv6_lkup = [_.pop("lookup_ipv6") if isinstance(_, dict) else _ for _ in url_lkup]
-                lookup_json[urls[i]] = url_lkups
-                full_lkups[urls[i]] = (ipv4_lkup, ipv6_lkup)
-            # lookup_json = {urls[i]: lookups[i] for i in range(len(urls))}
-            res_json['lookups'] = lookup_json
-            # results.append(res_json)
-            res_json2 = {}
-            res_json2['ID'] = id_
-            res_json2['lookups'] = full_lkups
-            f.write(f'{res_json}\n')
-            f2.write(f'{res_json2}\n'.encode())
-    lookup_timer = succ_timer + failed_timer    
-    logging.info(f'Total time: {lookup_timer:.4f} sec. performing lookups for {len(cert_url_dict)} certs.')
-    logging.info(f'Total of {succ_lookups} successful ({succ_timer:.3f} sec.) and {failed_lookups} ({failed_timer:.3f} sec.) failed lookups.')
-    end1 = time.time()
-    print(f'Total lookup time: {(end1 - start1):.4f} sec')
-    return results
+    logging.info(f'Parsed {len(certs_seen)} unique certs from log in {(stop - start):.4f} seconds.')
+    return out_q
 
 
 # multithreading steps:
 # 1. read all certs to a dict
 # 2. worker thread performs lookups
 # 3. worker thread writes lookup results to log
-
-def do_cert_lookup(cert):
-    pass
-    
 
 # one certificate per line: multiple domains (easier for certificate-level stats)
 # send output email 
@@ -178,31 +114,138 @@ def process_daily_log(args):
         log_file = sys.argv[1]
     else:
         log_file = LOG_FILE
+
+    print(f'Performing lookups for log file {log_file}')
     logging.info(f'Performing lookups for log file {log_file}')
     session = boto3.Session(profile_name=CRED_PROFILE)
+    m = mp.Manager()  # TODO: enable multiprocessing with a flag
+
     read_log_from_bucket(session, log_file)
-    certs = parse(MAX_COUNT)
-    lookup_res = resolve_dns(certs)
-    logging.info('Successfuly performed DNS lookups')
+    certs = parse(m, MAX_COUNT)
+    lookup_res = resolve_dns(m, certs)
+    write_logs_to_bucket(session)
+    logging.info('Successfuly logged DNS lookups.')
     return 0
+
+
+def get_lookups(id_, urls):
+
+    successful = 0
+    failed = 0
+    lookups = []
+    for url in urls:
+        domain_lookups = []
+        for iter in range(NUM_REPEAT_LKUPS):
+            try:
+                lookup = dns_resolver.perform_full_name_lookup(url)
+                domain_lookups.append(lookup)
+                successful += 1
+            except Exception as e:
+                print(f'Failed to resolve domain {url} for ID {id_}: {str(e)}')
+                logging.debug(f'Failed to resolve domain {url} for ID {id_}: {str(e)}')
+                domain_lookups.append(str(e))
+                failed += 1
+            
+        lookups.append(domain_lookups)
+
+    summary = {}
+    full_lkups = {}
+    for i in range(len(lookups)):
+        url_lkup = lookups[i]
+        ipv4_lkup = [_.pop("lookup_ipv4") if isinstance(_, dict) else _ for _ in url_lkup]
+        ipv6_lkup = [_.pop("lookup_ipv6") if isinstance(_, dict) else _ for _ in url_lkup]
+        summary[urls[i]] = url_lkup
+        full_lkups[urls[i]] = (ipv4_lkup, ipv6_lkup)
+
+    lookup_result = {"ID": id_, "summary": summary, "full": full_lkups}
+    return (lookup_result, successful, failed)
 
 
 def worker(in_q, out_q):
     # in_q: contains certificates that need to be looked up
     # out_q: contains lookup results
     name = os.getpid()
+    successful = 0
+    failed = 0
+    start = time.time()
+    lookups_done = []
+    print('in worker thread')
     while True:
-        item_cert = in_q.get()
-        if item_cert is None:
+        cert = in_q.get()
+        if cert is None:
             print(f'Worker {name} exiting: queue empty')
-            
-        lookup = resolve_dns(item_cert)
+            break
+        cert_id, cert_urls = cert
+        lookup, succ_lkup, fail_lkup = get_lookups(cert_id, cert_urls)
+        successful += succ_lkup
+        failed += fail_lkup
+        out_q.put(lookup)
+        lookups_done.append(lookup)
+        in_q.task_done()
+    end = time.time()
+    print(f'Time for {name} to perform {successful + failed} lookups: {(end-start):.4f} sec.')
+    return (successful, failed)
 
 
 def listener(write_q):
     # listens for lookup messages on the queue and writes to file
-    global counter
-    pass
+    with open(RES_FILE, "a") as f, gzip.open(FULL_LKUP_FILE, "wb") as f2:
+        finished = False
+        while not finished:
+            print('listening...')
+            msg = write_q.get()
+            if msg == 'end': # all done!
+                f.close()
+                finished = True
+                write_q.task_done()
+                # break
+            else:
+                lookup = msg
+                stats = {"ID": lookup["ID"], "summary": lookup["summary"]}
+                full_graph = {"ID": lookup["ID"], "full": lookup["full"]}
+                f.write(str(stats) + '\n')
+                f.flush()
+                f2.write((str(full_graph) + '\n').encode())
+                f2.flush()             
+                write_q.task_done()
+
+
+def resolve_dns(manager, cert_q):
+
+    print('starting resolve dns')
+    start = time.time()
+    lookup_q = manager.Queue()
+
+    max_proc = mp.cpu_count() + 2
+
+    pool = mp.Pool(max_proc)
+
+    watcher = pool.apply_async(listener, (lookup_q,))
+    num_tasks = 10  # roughly do 10 lookups a task
+
+    for i in range(num_tasks):
+        cert_q.put(None)  # sentinel
+
+    jobs = [pool.apply_async(worker, (cert_q, lookup_q)) for _ in range(num_tasks)]
+
+    all_lookups = []
+    for job in jobs:
+        all_lookups.append(job.get())
+
+    lookups_success, lookups_failed = [sum(i) for i in zip(*all_lookups)]
+
+    lookup_q.put('end')
+    pool.close()
+    pool.join()
+    end = time.time()
+    tot = lookups_success + lookups_failed
+
+    logging.info(f'Lookups completed: {lookups_success} successful {lookups_failed} failed')
+    logging.info(f'{tot} lookups completed in {(end-start):.4f} seconds.')
+
+    print(f'All the lookups done: {lookups_success} successful {lookups_failed} failed')
+    print(f'Time for all tasks to finish ({MAX_COUNT} lkups): {(end-start):.4f}')
+    return all_lookups
 
 
 if __name__ == '__main__':
