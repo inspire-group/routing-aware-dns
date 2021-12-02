2a1114dedf3c41e8b2f12fee5357a7dbc4f747f5
Henry Birge-Lee <henrybirgelee@yahoo.com>
2021-12-01 20:26:42 -0500

    Updates.

--- lookups_summary_to_csv.py
+++ lookups_summary_to_csv.py
@@ -15,11 +15,14 @@ def parse_args():
                         default="./lookups_summary.txt")
     parser.add_argument("-r", "--repetitive_lookups",
                         default=str(sys.maxsize))
-    parser.add_argument("-v", "--vantage_point", required=True)
+    parser.add_argument("-v", "--vantage_point", default="") # In the default case, we can extract the vp name from the filename.
     return parser.parse_args()
 
 
 def main(args):
+	vantagePoint = args.vantage_point
+	if vantagePoint == "":
+		vantagePoint = args.lookups_summary_file.split("/")[-2]
 	repetative_lookup_count = int(args.repetitive_lookups)
 	for line in open(args.lookups_summary_file):
 		sline = line.strip()
@@ -27,7 +30,9 @@ def main(args):
 			continue
 		try:
 			summary_object = json.loads(line)["summary"]
-			for domain in summary_object:
+			for domain in sorted(summary_object.keys()): # sorting the keys might be overkill, but in theory a dict makes no gaurantee of iteration order.
+				if domain == "soa_serial" or domain == "backup_soa_serial": # Ignore soa info since it is not a domain.
+					continue
 				domain_target_dns_ips = set()
 				domain_target_a_ips = set()
 				domain_target_dns_ips_v6 = set()
@@ -39,11 +44,11 @@ def main(args):
 					lookup_info = lookup[1] # The first element is the timestamp, so we want to lookup info in element 2.
 					if isinstance(lookup_info, str): # This is the case where the lookup is an error, we should continue.
 						continue
-					domain_target_a_ips = domain_target_a_ips.union(set(lookup_info["a_records"]))
+					domain_target_a_ips = domain_target_a_ips.union(set(lookup_info["a_records"][0]))
 					domain_target_dns_ips = domain_target_dns_ips.union(set(lookup_info["dns_targ_ipv4"]))
-					domain_target_aaaa_ips = domain_target_aaaa_ips.union(set(lookup_info["aaaa_records"]))
+					domain_target_aaaa_ips = domain_target_aaaa_ips.union(set(lookup_info["aaaa_records"][0]))
 					domain_target_dns_ips_v6 = domain_target_dns_ips_v6.union(set(lookup_info["dns_targ_ipv6"]))
-				print(f"{domain},{args.vantage_point},{' '.join(domain_target_a_ips)},{' '.join(domain_target_aaaa_ips)},{' '.join(domain_target_dns_ips)},{' '.join(domain_target_dns_ips_v6)}")
+				print(f"{domain},{vantagePoint},{' '.join(domain_target_a_ips)},{' '.join(domain_target_aaaa_ips)},{' '.join(domain_target_dns_ips)},{' '.join(domain_target_dns_ips_v6)}")
 
 
 
